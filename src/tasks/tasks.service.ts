56f756a2cb0ec1dfcc17a8f46bd4d0d9a50ea5f3
Mobin Shaterian <mobin.shaterian@wallit.eu>
2024-06-17 00:24:03 +0330

    [Add] get all tasks

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks = [];
+
+  getAllTasks() {
+    this.tasks.push({ id: 1, title: 'Task 1', description: 'Description 1' });
+    return this.tasks;
+  }
+}
