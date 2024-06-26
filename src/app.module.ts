19da554166719954086d15391424f61b1d1d2e33
Mobin Shaterian <mobin.shaterian@wallit.eu>
2024-06-27 00:08:51 +0330

    [Add] postgres module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '127.0.0.1',
+      port: 3001,
+      username: 'user',
+      password: 'pass',
+      database: 'task',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
