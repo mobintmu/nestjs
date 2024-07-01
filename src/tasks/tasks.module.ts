60c078cd19e4694dd9a2da6be943b8e3e5d49466
Mobin Shaterian <mobinest@gmail.com>
2024-07-01 16:55:27 +0330

    AuthGuard

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -4,9 +4,10 @@ import { TasksService } from './tasks.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Tasks } from './task.entity';
 import { TaskRepository } from './task.repository';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Tasks])],
+  imports: [TypeOrmModule.forFeature([Tasks]), AuthModule],
   controllers: [TasksController],
   providers: [TasksService, TaskRepository],
 })
