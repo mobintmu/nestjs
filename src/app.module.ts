372dc40efc5aec937065dd121c334bbca3bb0aa8
Mobin Shaterian <mobinest@gmail.com>
2024-07-04 15:51:23 +0330

    [Add] .en async

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,22 +2,28 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: [`.env.stage.${process.env.STAGE}`],
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: '127.0.0.1',
-      port: 3002,
-      username: 'user',
-      password: 'pass',
-      database: 'task',
-      autoLoadEntities: true,
-      // synchronize: true,
+    // Add ConfigModule to the imports array
+    ConfigModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: parseInt(configService.get('DB_PORT')),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASS'),
+        database: configService.get('DB_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: false,
+      }),
     }),
     TasksModule,
     AuthModule,
